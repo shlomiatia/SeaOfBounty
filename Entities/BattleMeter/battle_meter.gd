525768bdee66dc8ad63ab5286519ba61929721fa
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2025-11-04 19:07:57 +0200

    cp

---
+++ Entities/BattleMeter/battle_meter.gd
@@ -0,0 +1,48 @@
+class_name BattleMeter extends Node2D
+
+signal indicator_stopped(value: float)
+
+@onready var indicator = $Indicator
+
+var tween: Tween
+
+func _ready() -> void:
+    animate_indicator()
+
+func _input(event: InputEvent) -> void:
+    if event is InputEventMouseButton and event.pressed:
+        if !tween:
+            return
+        stop_indicator()
+
+func animate_indicator() -> void:
+    var start_pos = Vector2(0, -55)
+    indicator.position = start_pos
+    var end_pos = start_pos + Vector2(0, 110)
+
+    tween = create_tween()
+    tween.set_loops()
+    tween.tween_property(indicator, "position", end_pos, 1.0).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
+    tween.tween_property(indicator, "position", start_pos, 1.0).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
+
+
+func stop_indicator() -> void:
+    if tween:
+        tween.kill()
+    tween = null
+
+    var center_pos = 0
+    var current_pos = indicator.position.y
+    var distance_from_center = abs(current_pos - center_pos)
+
+    var value := 0.0
+    if distance_from_center <= 4:
+        value = 1.5
+    elif distance_from_center <= 15:
+        value = 1.0
+    elif distance_from_center <= 20:
+        value = 0.5
+    else:
+        value = 0.0
+
+    indicator_stopped.emit(value)
