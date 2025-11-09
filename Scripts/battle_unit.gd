0de4267a047e092a82b0aae431fa8cbda80d7b37
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2025-11-09 19:46:33 +0200

    cp

--- Scripts/battle_unit.gd
+++ Scripts/battle_unit.gd
@@ -2,11 +2,16 @@ class_name BattleUnit extends Node2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+func _ready() -> void:
+    animation_player.animation_finished.connect(_on_animation_finished)
+
 func start() -> void:
     animation_player.play("MoveToBeforeAttack")
-    await animation_player.animation_finished
-    animation_player.play("BeforeAttack")
 
 func attack() -> void:
     animation_player.play("Attack")
     await animation_player.animation_finished
+
+func _on_animation_finished(animation: String) -> void:
+    if animation == "MoveToBeforeAttack":
+        animation_player.play("BeforeAttack")
