20259403b999ba41532c92ceccef1968f122392c
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2025-11-09 22:20:20 +0200

    cp

--- Entities/Battle/battle.gd
+++ Entities/Battle/battle.gd
@@ -3,8 +3,10 @@ class_name Battle extends Node2D
 @onready var label = $Label
 @onready var battle_meter = $BattleMeter
 @onready var hero_placeholder = $Hero
+@onready var hero_hp_label: Label = $Hero/HeroHP
+@onready var enemy_hp_label: Label = $Enemy/EnemyHP
 @onready var map: Map = $"../Map"
-
+@onready var shaking_camera: Camera2D = $"../ShakingCamera"
 
 func start(attacker: Unit, defender: Unit) -> void:
     visible = true
@@ -12,14 +14,23 @@ func start(attacker: Unit, defender: Unit) -> void:
     var defender_is_hero := defender.is_in_group("heroes")
     var can_counter_attack := _can_counter_attack(attacker, defender)
     var hero_name: String
+    var hero: Unit
+    var enemy: Unit
+
     if defender_is_hero:
         hero_name = defender.name
+        hero = defender
+        enemy = attacker
     else:
         hero_name = attacker.name
+        hero = attacker
+        enemy = defender
+
+    hero_hp_label.text = str(hero.hp)
+    enemy_hp_label.text = str(enemy.hp)
     
-    var ass = "res://Entities/%s/%s.tscn" % [hero_name, hero_name]
-    prints(ass, hero_name)
-    var battle_hero: BattleUnit = load(ass).instantiate() as BattleUnit
+    var unit = "res://Entities/%s/%s.tscn" % [hero_name, hero_name]
+    var battle_hero: BattleUnit = load(unit).instantiate() as BattleUnit
     hero_placeholder.add_child(battle_hero)
     
     if defender_is_hero:
@@ -46,11 +57,12 @@ func _perform_attack(hero: Unit, enemy: Unit, battle_hero: BattleUnit) -> void:
     var attack_value = await battle_meter.indicator_stopped
 
     var damage_dealt = int(hero.damage * attack_value)
+    var old_enemy_hp = enemy.hp
     enemy.hp = max(0, enemy.hp - damage_dealt)
 
-    label.text = "Inflicted " + str(damage_dealt) + " damage"
+    animate_hp_change(enemy_hp_label, old_enemy_hp, enemy.hp)
     await battle_hero.attack()
-    
+
     if enemy.hp == 0:
         await _fade_out_and_remove(enemy)
 
@@ -61,10 +73,10 @@ func _perform_defend(hero: Unit, enemy: Unit) -> void:
     var defend_value = await battle_meter.indicator_stopped
 
     var damage_dealt = int(enemy.damage * defend_value)
+    var old_hero_hp = hero.hp
     hero.hp = max(0, hero.hp - damage_dealt)
 
-    label.text = "Took " + str(damage_dealt) + " damage"
-    await get_tree().create_timer(1.0).timeout
+    await animate_hp_change(hero_hp_label, old_hero_hp, hero.hp)
 
     if hero.hp == 0:
         await _fade_out_and_remove(hero)
@@ -75,3 +87,25 @@ func _fade_out_and_remove(unit: Unit) -> void:
     tween.tween_property(unit, "modulate:a", 0.0, 1.0)
     await tween.finished
     unit.queue_free()
+
+func animate_hp_change(hp_label: Label, old_hp: int, new_hp: int) -> void:
+    label.text = ""
+    var duration = 0.2
+    var steps = abs(new_hp - old_hp)
+
+    if steps == 0:
+        return
+
+    shaking_camera.start_screen_shake()
+
+    var time_per_step = duration / steps
+    var current_hp = old_hp
+
+    while current_hp != new_hp:
+        if old_hp > new_hp:
+            current_hp -= 1
+        else:
+            current_hp += 1
+
+        hp_label.text = str(current_hp)
+        await get_tree().create_timer(time_per_step).timeout
