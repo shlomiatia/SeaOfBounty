2cd72d039d5b4c70a0493d8fed40def74f7699bf
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2025-11-10 16:54:25 +0200

    cp

--- Entities/Battle/battle.gd
+++ Entities/Battle/battle.gd
@@ -41,8 +41,9 @@ func start(attacker: Unit, defender: Unit) -> void:
         await _perform_attack(attacker, defender, battle_hero)
         if attacker.hp > 0 and defender.hp > 0 and can_counter_attack:
             await _perform_defend(attacker, defender)
+        
+    await get_tree().create_timer(0.2).timeout
 
-    await get_tree().create_timer(0.5).timeout
     visible = false
     hero_placeholder.remove_child(battle_hero)
 
@@ -56,11 +57,7 @@ func _perform_attack(hero: Unit, enemy: Unit, battle_hero: BattleUnit) -> void:
     battle_meter.start("attack")
     var attack_value = await battle_meter.indicator_stopped
 
-    var damage_dealt = int(hero.damage * attack_value)
-    var old_enemy_hp = enemy.hp
-    enemy.hp = max(0, enemy.hp - damage_dealt)
-
-    animate_hp_change(enemy_hp_label, old_enemy_hp, enemy.hp)
+    assign_damage(hero, enemy, enemy_hp_label, attack_value)
     await battle_hero.attack()
 
     if enemy.hp == 0:
@@ -72,11 +69,7 @@ func _perform_defend(hero: Unit, enemy: Unit) -> void:
     battle_meter.start("defense")
     var defend_value = await battle_meter.indicator_stopped
 
-    var damage_dealt = int(enemy.damage * defend_value)
-    var old_hero_hp = hero.hp
-    hero.hp = max(0, hero.hp - damage_dealt)
-
-    await animate_hp_change(hero_hp_label, old_hero_hp, hero.hp)
+    await assign_damage(enemy, hero, hero_hp_label, defend_value)
 
     if hero.hp == 0:
         await _fade_out_and_remove(hero)
@@ -88,7 +81,12 @@ func _fade_out_and_remove(unit: Unit) -> void:
     await tween.finished
     unit.queue_free()
 
-func animate_hp_change(hp_label: Label, old_hp: int, new_hp: int) -> void:
+func assign_damage(attacker: Unit, defender: Unit, hp_label: Label, modifier: float) -> void:
+    var damage_dealt = int(attacker.damage * modifier)
+    var old_hp = defender.hp
+    defender.hp = max(0, defender.hp - damage_dealt)
+    var new_hp = defender.hp
+
     label.text = ""
     var duration = 0.2
     var steps = abs(new_hp - old_hp)
