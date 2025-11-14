9032913068b6acb00cf06656aa5b06bf86e29423
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2025-11-14 11:13:05 +0200

    cp

--- Entities/Unit/unit.gd
+++ Entities/Unit/unit.gd
@@ -10,6 +10,7 @@ const movement_speed: float = 300.0
 @export var max_movement: int = 4
 @export var attack_range: int = 1
 @export var initial_direction: String = "right"
+@export var sprite_frames: SpriteFrames
 
 @onready var map: Map = $"../../Map"
 @onready var animated_sprite_2d = $AnimatedSprite2D
@@ -29,64 +30,14 @@ var text_list: Array[String] = []
 var current_text_index: int = 0
 
 func _ready() -> void:
+    if sprite_frames:
+        animated_sprite_2d.sprite_frames = sprite_frames
+        reflection.sprite_frames = sprite_frames
+
     position = map.map_to_local(map.local_to_map(position))
-    setup_unit_frames()
     animated_sprite_2d.play(initial_direction)
     reflection.play(initial_direction)
     update_reflection_uv_bounds()
-
-func setup_unit_frames() -> void:
-    var frame_coords = get_frame_coordinates(name)
-
-    if frame_coords.is_empty():
-        return
-    
-    var base_texture = preload("res://Textures/Smaller boat characters.png")
-    var sprite_frames = animated_sprite_2d.sprite_frames
-
-    var directions = ["up", "down", "right", "left"]
-    for i in range(4):
-        var atlas = AtlasTexture.new()
-        atlas.atlas = base_texture
-        atlas.region = Rect2(frame_coords[i].x, frame_coords[i].y, 40, 40)
-
-        sprite_frames.set_frame(directions[i], 0, atlas)
-
-    reflection.sprite_frames = sprite_frames
-
-func get_frame_coordinates(character_name: String) -> Array:
-    var coords = []
-    var start_x = 0
-    var start_y = 0
-
-    match character_name:
-        "Musketeer":
-            start_x = 1
-            start_y = 1
-        "OneEye":
-            start_x = 165
-            start_y = 1
-        "HealerMage":
-            start_x = 1
-            start_y = 54
-        "FireMage":
-            start_x = 165
-            start_y = 54
-        "Fisherman":
-            start_x = 1
-            start_y = 107
-        "Orphan":
-            start_x = 165
-            start_y = 107
-        _:
-            return []
-
-    coords.append(Vector2(start_x, start_y))
-    coords.append(Vector2(start_x + 41, start_y))
-    coords.append(Vector2(start_x + 82, start_y))
-    coords.append(Vector2(start_x + 123, start_y))
-
-    return coords
 
 func _process(_delta: float) -> void:
     status_label.text = "%s\n%s/%s" % [name, hp, max_hp]
@@ -158,9 +109,9 @@ func get_animation(direction: Vector2) -> String:
 
 func update_reflection_uv_bounds() -> void:
     var current_animation = reflection.animation
-    var sprite_frames = reflection.sprite_frames
+    var reflection_sprite_frames = reflection.sprite_frames
 
-    var frame_texture = sprite_frames.get_frame_texture(current_animation, 0)
+    var frame_texture = reflection_sprite_frames.get_frame_texture(current_animation, 0)
 
     var atlas_texture = frame_texture as AtlasTexture
     var atlas = atlas_texture.atlas
