ae7ae736fb8811a34609be614bda53610c16f35c
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2025-11-19 10:48:19 +0200

    cp

--- Levels/Level4/level_4.gd
+++ Levels/Level4/level_4.gd
@@ -1,6 +1,21 @@
 class_name Level4 extends Node2D
 
+@onready var orphan: Unit = $Main/Units/Orphan
+@onready var finn: Unit = $Main/Units/Fisherman
+@onready var one_eye: Unit = $Main/Units/OneEye
+
 func _ready() -> void:
     MusicPlayer.stream = preload("res://Music/Drums B.mp3")
     MusicPlayer.play()
+    await get_tree().create_timer(1.0).timeout
+
+    orphan.start_text_list(["There are so many!"])
+    await orphan.text_list_finished
+
+    one_eye.start_text_list(["That's our only way out."])
+    await one_eye.text_list_finished
+
+    finn.start_text_list(["Then we fight!"])
+    await finn.text_list_finished
+
     $Main.start_player_turn()
