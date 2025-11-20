f2452474589927d661cb84c342f192f02ff0208f
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2025-11-20 15:40:58 +0200

    cp

--- Entities/EyeCandies/bird_shadow.gd
+++ Entities/EyeCandies/bird_shadow.gd
@@ -70,11 +70,15 @@ func _start() -> void:
     if _velocity.x > 0:
         flip_h = true
         _bird_child.flip_h = true
-        _bird_child.position = Vector2(-x_offset, -y_offset)
+        # Bird is in front (positive offset), shadow moves forward to keep bird off-screen
+        _bird_child.position = Vector2(x_offset, -y_offset)
+        position.x += x_offset
     else:
         flip_h = false
         _bird_child.flip_h = false
-        _bird_child.position = Vector2(x_offset, -y_offset)
+        # Bird is in front (negative offset), shadow moves backward to keep bird off-screen
+        _bird_child.position = Vector2(-x_offset, -y_offset)
+        position.x -= x_offset
 
     _active = true
 
