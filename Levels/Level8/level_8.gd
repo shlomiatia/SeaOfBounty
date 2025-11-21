44f104c8b15b25f1dffdeddfad824b4654c98ecd
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2025-11-21 17:43:55 +0200

    cp

--- Levels/Level8/level_8.gd
+++ Levels/Level8/level_8.gd
@@ -1,8 +1,22 @@
 class_name Level8 extends Node2D
 
+@onready var main: Main = $Main
+@onready var miguel: Unit = $Main/Units/Miguel
+@onready var constantine: Unit = $Main/Units/Constantine
+@onready var tutorial_event_handler: TutorialEventHandler8 = $TutorialEventHandler
+
 func _ready() -> void:
     MusicPlayer.stream = preload("res://Music/Flute B.mp3")
     MusicPlayer.play()
     await get_tree().create_timer(1.0).timeout
 
-    $Main.start_player_turn()
+    miguel.start_text_list(["Charge my friend!, charge!!!"])
+    await miguel.text_list_finished
+    await get_tree().process_frame
+
+    constantine.start_text_list(["Pfff, sometimes the best strategy is to fall back."])
+    await constantine.text_list_finished
+    await get_tree().process_frame
+
+    main.start_player_turn()
+    tutorial_event_handler.show_tutorial_at_step(0)
---
+++ Levels/Level8/tutorial_event_handler_8.gd
@@ -0,0 +1,80 @@
+class_name TutorialEventHandler8 extends ColorRect
+
+@onready var tutorial: Node2D = $"../Tutorial"
+@onready var typing_label: TypingLabel = $"../Tutorial/TypingLabel"
+@onready var map: Map = $"../Main/Map"
+@onready var main: Main = $"../Main"
+@onready var tutorial_highlight: TileMapLayer = $"../TutorialHighlight"
+@onready var tutorial_label: TypingLabel = $"../TutorialLabel"
+
+var tutorial_active: bool = false
+var tutorial_step: int = 0
+var tutorial_cells: Array[Vector2i] = [
+	Vector2i(0, 0),
+]
+
+var skip_pressed: bool = false
+
+func _input(event: InputEvent) -> void:
+	if not tutorial_active:
+		return
+
+	if event.is_action_pressed("cancel"):
+		get_viewport().set_input_as_handled()
+
+	if tutorial_step == 0:
+		if event.is_action_pressed("confirm"):
+			var clicked_pos = main.cursor.position
+			var clicked_cell = map.local_to_map(clicked_pos)
+
+			if clicked_cell == tutorial_cells[tutorial_step]:
+				tutorial_active = false
+				tutorial_highlight.clear()
+				show_tutorial_at_step(1)
+			else:
+				get_viewport().set_input_as_handled()
+				typing_label.text = "Click me!"
+	elif tutorial_step == 1:
+		if event.is_action_pressed("skip"):
+			skip_pressed = true
+			tutorial_active = false
+			tutorial_highlight.clear()
+			tutorial.visible = false
+			tutorial_label.text = ""
+			get_viewport().set_input_as_handled()
+
+func get_tutorial_text(step: int) -> String:
+	match step:
+		0:
+			return "Select Constantine."
+		1:
+			return "Press %s to skip hero turn." % [LastInput.get_text("Middle mouse button", "Backspace", "Y button")]
+		_:
+			return ""
+
+func show_tutorial_at_step(step: int) -> void:
+	tutorial_step = step
+
+	if step == 0:
+		var target_position = map.map_to_local(tutorial_cells[step])
+		tutorial.position = target_position
+		tutorial.visible = true
+		tutorial_active = true
+		typing_label.text = ""
+
+		set_tutorial_highlight(tutorial_cells[step])
+	elif step == 1:
+		tutorial.visible = false
+		tutorial_active = true
+		set_tutorial_highlight(Vector2i.MIN)
+
+	tutorial_label.text = get_tutorial_text(step)
+
+func set_tutorial_highlight(cell_to_highlight: Vector2i) -> void:
+	tutorial_highlight.clear()
+	var map_rect = map.get_used_rect()
+	for x in range(map_rect.position.x, map_rect.position.x + map_rect.size.x):
+		for y in range(map_rect.position.y, map_rect.position.y + map_rect.size.y):
+			var cell = Vector2i(x, y)
+			if cell != cell_to_highlight:
+				tutorial_highlight.set_cell(cell, 0, Vector2i(2, 0))
