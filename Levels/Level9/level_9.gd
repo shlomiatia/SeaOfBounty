b4fb7abdaadb884e15cc9ee1c0e82e3ca39438d8
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2025-11-22 17:20:15 +0200

    aa

---
+++ Levels/Level9/level_9.gd
@@ -0,0 +1,35 @@
+class_name Level9 extends Node2D
+
+@onready var dialog: Dialog = $Dialog
+@onready var fade: Fade = $CanvasLayer/Fade
+@onready var background: Sprite2D = $Background
+var stage := 0
+
+func _ready() -> void:
+	await get_tree().create_timer(1.0).timeout
+	start()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("cancel"):
+		get_tree().change_scene_to_file("res://Levels/Level10/Level10.tscn")
+
+func start() -> void:
+	stage = 1
+	MusicPlayer.stream = preload("res://Music/Violin D.mp3")
+	MusicPlayer.play()
+	var array: Array[Array] = [
+		["", "The north."],
+		["Miguel", "Whoa! Who are you milady?"],
+		["Lia", "You're not monsters...\nGood. I had enough of those."],
+		["Constantine", "Do you know why this place is vomiting horrors into the world?"],
+		["Lia", "The monsters aren't attacking...\nThey're fleeing."],
+		["Lia", "My group encountered this colossal monster...\nAn iceberg broke and created a tsunamy..."],
+		["Lia", "I don't know who made it."],
+		["Constantine", "Lovely. Cataclysmic wave, terrified beasts, missing expedition..."],
+		["Miguel", "...and three gorgeous idiots marching straight into it."]
+	]
+	dialog.start(array)
+	dialog.dialog_finished.connect(_on_dialog_finished)
+
+func _on_dialog_finished() -> void:
+	get_tree().change_scene_to_file("res://Levels/Level10/Level10.tscn")
