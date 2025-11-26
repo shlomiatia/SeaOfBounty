fd11ef3b0f3a29b344e274e8cb1009233eb74712
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2025-11-26 15:50:32 +0200

    בפ

--- Entities/Battle/battle.gd
+++ Entities/Battle/battle.gd
@@ -70,8 +70,17 @@ func start(attacker: Unit, defender: Unit) -> void:
                 if tentacle.display_name == "Tentacle":
                     tentacle.hp = 0
                     _fade_out_and_remove(tentacle)
+        if enemy.name == "SeaHorseHead" or enemy.name == "SeaHorseBody" or enemy.name == "SeaHorseTail":
+            for sea_horse_part in get_tree().get_nodes_in_group("enemies"):
+                if sea_horse_part.name == "SeaHorseHead" or sea_horse_part.name == "SeaHorseBody" or sea_horse_part.name == "SeaHorseTail":
+                    sea_horse_part.hp = 0
+                    _fade_out_and_remove(sea_horse_part)
         await _fade_out_and_remove(enemy)
-    
+    elif enemy.name == "SeaHorseHead" or enemy.name == "SeaHorseBody" or enemy.name == "SeaHorseTail":
+        for sea_horse_part in get_tree().get_nodes_in_group("enemies"):
+            if sea_horse_part.name == "SeaHorseHead" or sea_horse_part.name == "SeaHorseBody" or sea_horse_part.name == "SeaHorseTail":
+                sea_horse_part.hp = enemy.hp
+
     if hero.hp == 0:
         await _fade_out_and_remove(hero)
 
